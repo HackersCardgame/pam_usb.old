6cd79a1c48647f8cb0200022eecda3f23d58664c
HackersCardgame.ch <74041568+HackersCardgame@users.noreply.github.com>
2022-02-09 05:19:36 +0100

    Update prerequisite.sh

--- prerequisite.sh
+++ prerequisite.sh
@@ -4,4 +4,4 @@ sudo apt-get install build-essential
 
 sudo apt install libxml2-dev libpam0g-dev libudisks2-dev libglib2.0-dev gir1.2-udisks-2.0 python3 python3-gi
 
-sudo apt-get install debuild
+sudo apt-get install devscripts
