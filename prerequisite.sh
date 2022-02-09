f019587e1dc4f9ffc7b4c785fa777d47cf5cbf48
HackersCardgame.ch <74041568+HackersCardgame@users.noreply.github.com>
2022-02-09 05:27:04 +0100

    Update prerequisite.sh

--- prerequisite.sh
+++ prerequisite.sh
@@ -5,3 +5,5 @@ sudo apt-get install build-essential
 sudo apt install libxml2-dev libpam0g-dev libudisks2-dev libglib2.0-dev gir1.2-udisks-2.0 python3 python3-gi
 
 sudo apt-get install devscripts
+
+sudo apt-get install gawk
